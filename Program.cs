60bd4d0d545eeadd377d2c63ffc6667614a91f9b
Filip392-20 <fildahavel08@gmail.com>
2026-06-08 21:32:13 +0200

    Create Program.cs
    Přidání Program.cs – vstupní bod aplikace
    
    Program.cs obsahuje metodu Main, kterou se celá aplikace spouští.
    
    - BuildAvaloniaApp() nastaví a sestaví Avalonia aplikaci
    (App), zvolí platformu (UsePlatformDetect), načte font Inter
    a zapne logování
    - Main() je označená [STAThread] (vyžadováno pro desktopové GUI)
    a spustí aplikaci v klasickém desktopovém režimu

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using Avalonia;
+using System;
+
+namespace InventarniSystem;
+
+// Vstupní bod programu. Spustí Avalonia aplikaci.
+class Program
+{
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
+}
